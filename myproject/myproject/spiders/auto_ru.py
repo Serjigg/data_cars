29f6d01b9cc52d8b34c02cc9d3d2a7703f8ef7d3
Serg <sviskov2@gmail.com>
2025-09-20 21:31:07 +0300

    собирает данные об отзыве: - имя автора - рейтинг - позитивные качества - отрицательные качества - марка - модель - поколение - текст автора (отзыв) - комментарии к этому отзывы

--- myproject/myproject/spiders/auto_ru.py
+++ myproject/myproject/spiders/auto_ru.py
@@ -1,41 +1,47 @@
-import time
-
 import scrapy
-import csv
-import os
 
 
-class MySpider(scrapy.Spider):
-    """ Собирает ссылки на отзывы """
-    name = 'myspider'
-    output_file = 'cars2.csv'
-
-    def __init__(self, *args, **kwargs):
-        super(MySpider, self).__init__(*args, **kwargs)
-        with open(self.output_file, 'w', newline='', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerow(['page', 'link_number', 'url', 'full_url'])
-
-    def start_requests(self):
-        for page in range(902, 9600):
-            url = f'https://auto.ru/logbook/reviews/cars/?page={page}'
-            time.sleep(0.25)
-            yield scrapy.Request(url=url, callback=self.parse, meta={'page': page})
+class ExampleSpider(scrapy.Spider):
+    name = 'auto'
+    allowed_domains = ['auto.ru']
+    start_urls = [
+        'https://auto.ru/logbook/review/cars/jaguar/xf/7771114/ochen-nedootsenennaia-marka_72200147-86e4-4c3a-9d81-f2bc26db8a6e/']
 
     def parse(self, response):
-        page = response.meta['page']
-        links = response.css('a.Link.LogbookListingSnippet__clicker-MqFUR::attr(href)').getall()
+        # Correct title extraction
+        #mark = response.xpath('.BreadCrumbs__item ::text').get() # TODO
 
-        with open(self.output_file, 'a', newline='', encoding='utf-8') as f:
-            writer = csv.writer(f)
-            for i, link in enumerate(links, 1):
-                full_url = response.urljoin(link)
-                writer.writerow([page, i, link, full_url])
-                print(f"Сохранено: {full_url}")
+        mark_mod_gen = response.css('.BreadCrumbs__item ::text').getall()
+        mark = mark_mod_gen[3]
+        model = mark_mod_gen[4]
+        generation = mark_mod_gen[5]
+        #model_right = response.css('.ContentLinks__title-TdcWA ::text').get()
+        review_text = response.css('.LogbookCardContentItem__text-a85z7 ::text').getall()
+        comments_about_auto = response.css('.CommentsList__commentWrapper ::text').getall()
+        name_author = response.css('.LogbookVerified-DBngj ::text').get()
+        rating = response.css('.StarRate2__rating-e5yYY ::text').get()
+        positive_qualities = response.xpath('(//*[contains(@class, "LogbookCardRatingSummary__prosAndConsContent")])[1]//text()').get()
+        negative_qualities = response.xpath('(//*[contains(@class, "LogbookCardRatingSummary__prosAndConsContent")])[2]//text()').get()
 
-        print(f"Страница {page}: сохранено {len(links)} ссылок")
+        print(name_author)  # имя автора
+        print(rating)  # рейтинг
+        print(positive_qualities)
+        print(negative_qualities)
+        print(mark)
+        print(model)
+        print(generation)
+        print(review_text)
+        print(comments_about_auto)
+
+
+        #print(text)
+        # Alternative XPath approach (corrected syntax)
+        name = response.xpath("//div[contains(@class, 'LogbookCardAuthor__profile-ZAoW0')]/text()")
+
 
         yield {
-            'page': page,
-            'links_count': len(links)
+            'mark': model,
+            'author_css': name_author,
+            'author_xpath': name
+
         }
